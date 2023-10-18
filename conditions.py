5823691b702621040c28a7d301a5fb3dc20e42d9
Pekk-Juhasz Dora <pekk-juhaszdora@Pekk-Juhasz-MacBook-Air.local>
2023-10-18 20:16:38 +0200

    conditions.py and functions.py

---
+++ conditions.py
@@ -0,0 +1,9 @@
+a = 5
+b = 5
+
+if a ==b:
+    print ("a equivalents with b")
+elif a > b:
+    print ("a larger than b")
+else:
+    print ("b larger than a")
---
+++ functions.py
@@ -0,0 +1,17 @@
+def helloworld():
+    print ("Hello World!")
+
+helloworld()
+
+def sum(a, b):
+    print(a+ b)
+
+sum(2, 3)
+
+def multiple(a, b):
+    mul= a * b
+    return mul
+result = multiple(3, 45)
+
+print(result)
+
