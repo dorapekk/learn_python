3809aa9ee4e3c493b3c8600054c860b39c9285bd
Pekk-Juhasz Dora <pekk-juhaszdora@Pekk-Juhasz-MacBook-Air.local>
2023-11-01 20:01:34 +0100

    bonus task

--- homework_01.py
+++ homework_01.py
@@ -15,36 +15,48 @@ PEOPLE = [
 {
     "first_name": "Bubu",
     "last_name": "Szigethy",
-    "favourite": "whiskey",
+    "favourite": ["whiskey"],
  },
 {   
     "first_name": "Tasi",
     "last_name": "Tasnadi",
-    "favourite": "tequila",
+    "favourite": ["sake"],
 },
 {   
     "first_name": "Balazs",
     "last_name": "Bellanyi",
-    "favourite": "palinka",
+    "favourite": ["palinka"],
 },
 {   
     "first_name": "Dora",
     "last_name": "Pekk-Juhasz",
-    "favourite": "vodka",
+    "favourite": ["vodka", "tequila"],
 },
 {   
     "first_name": "Erika",
     "last_name": "Juhasz",
-    "favourite": "gin",
+    "favourite": ["gin"],
 }
 ]
-
-
 
 i = 0
 while i < len(PEOPLE):
     first_name = PEOPLE[i]["first_name"]
     last_name = PEOPLE[i]["last_name"]
+    favourite = PEOPLE[i]["favourite"]
     print(f"{first_name} {last_name} ")
-    i = i + 1
+    avaliable = False
+    j = 0
+    while j < len(favourite):
+        k = 0
+        while k < len(SHOTS):
+            if favourite[j] == SHOTS[k]:
+                avaliable = True
+            k = k + 1 
+        j = j + 1
 
+    if avaliable:
+        print("Favourite drink is avaliable")
+    else:
+        print("Favourite drink is unavaliable")
+    i = i + 1
